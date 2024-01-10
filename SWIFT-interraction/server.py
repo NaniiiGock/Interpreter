efd571fa3fe0e78a2fe34d1eb1e775d4f63010a7
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2024-01-10 05:23:43 +0200

    status code mapping to functions

--- SWIFT-interraction/server.py
+++ SWIFT-interraction/server.py
@@ -4,18 +4,43 @@ import json
 import pickle
 from StatusCodes import StatusCode
 from Communicator import Communicator
-from execution_handler import ExecutionHandler
-
-from response_container import LLMResponse
-from response_parser import ResponseParser
-
 
 ### TODO: rewrite
 def is_valid_status_code(code: int):
     return code in [0, 1, 2, 3, 4, 7, 8, 10, 11, 15, 16, 17, 18, 19]
 
 
+class StatusCodesMapper:
+    @staticmethod
+    async def submit_user_response(data, websocket):
+        response = {
+            **data,
+            "statusCode": StatusCode.REQUEST_SENT_TO_API,
+        }
+        await websocket.send(json.dumps(response))
+        asyncio.create_task(process_user_input(data, websocket))
+
+    @staticmethod
+    async def send_for_execution(data, websocket):
+        llm_message = b'\x80\x04\x95\x99\x01\x00\x00\x00\x00\x00\x00\x8c\rlitellm.utils\x94\x8c\x07Message\x94\x93\x94)\x81\x94}\x94(\x8c\x08__dict__\x94}\x94\x8c\x12__pydantic_extra__\x94}\x94(\x8c\x07content\x94N\x8c\x04role\x94\x8c\tassistant\x94\x8c\ntool_calls\x94]\x94h\x00\x8c\x1dChatCompletionMessageToolCall\x94\x93\x94)\x81\x94}\x94(h\x05}\x94(\x8c\x02id\x94\x8c\x1dcall_dcpfu8R4FYC21TsTPz5uLUxy\x94\x8c\x08function\x94h\x00\x8c\x08Function\x94\x93\x94)\x81\x94}\x94(h\x05}\x94(\x8c\targuments\x94\x8c\x16{\n  "name": "Safari"\n}\x94\x8c\x04name\x94\x8c\x08open_app\x94uh\x07}\x94\x8c\x17__pydantic_fields_set__\x94\x8f\x94(h\x1dh\x1b\x90\x8c\x14__pydantic_private__\x94Nub\x8c\x04type\x94\x8c\x08function\x94uh\x07}\x94h \x8f\x94(h\x15h#h\x13\x90h"Nubauh \x8f\x94h"Nub.'
+        llm_message = pickle.loads(llm_message)
+        statusCode = StatusCode.SENT_FOR_EXECUTION
+        response = {
+            **data,
+            "statusCode": statusCode,
+        }
+        print(llm_message)
+        await websocket.send(json.dumps(response))
+        asyncio.create_task(process_user_input(data, websocket, llm_message, statusCode))
+
+
 async def echo(websocket, path):
+    func_mapping = {
+        StatusCode.SUBMIT_USER_RESPONSE: StatusCodesMapper.submit_user_response,
+        StatusCode.EXECUTION_CONFIRMED: StatusCodesMapper.send_for_execution,
+        StatusCode.ASK_RERUN:  StatusCodesMapper.send_for_execution,
+    }
+
     async for message in websocket:
         data = json.loads(message)
         print("Received: ", data)
@@ -23,48 +48,9 @@ async def echo(websocket, path):
         statusCode = int(data["statusCode"])
         assert is_valid_status_code(statusCode), "Wrong StatusCode... :/"
 
-        if statusCode == StatusCode.SUBMIT_USER_RESPONSE:
-            response = {**data,
-                        **{
-                            "statusCode": StatusCode.REQUEST_SENT_TO_API,
-                        }
-                        }
-            await websocket.send(json.dumps(response))
-            asyncio.create_task(process_user_input(data, websocket))
-
-        if statusCode == StatusCode.EXECUTION_CONFIRMED:
-            # await llm_response = DBHandler.get_llm_response(data["UUID"])
-            llm_message = b'\x80\x04\x95\xc9\x01\x00\x00\x00\x00\x00\x00\x8c\rlitellm.utils\x94\x8c\x07Message\x94\x93\x94)\x81\x94}\x94(\x8c\x08__dict__\x94}\x94\x8c\x12__pydantic_extra__\x94}\x94(\x8c\x07content\x94N\x8c\x04role\x94\x8c\tassistant\x94\x8c\ntool_calls\x94]\x94h\x00\x8c\x1dChatCompletionMessageToolCall\x94\x93\x94)\x81\x94}\x94(h\x05}\x94(\x8c\x02id\x94\x8c\x1dcall_oIABKi3ICZCzUXuMhCMYD2pP\x94\x8c\x08function\x94h\x00\x8c\x08Function\x94\x93\x94)\x81\x94}\x94(h\x05}\x94(\x8c\targuments\x94\x8c7{\n  "name": "Anastasiia \xf0\x9f\x8c\xb8",\n  "body": "I love you"\n}\x94\x8c\x04name\x94\x8c\x17message_by_contact_name\x94uh\x07}\x94\x8c\x17__pydantic_fields_set__\x94\x8f\x94(h\x1dh\x1b\x90\x8c\x14__pydantic_private__\x94Nub\x8c\x04type\x94\x8c\x08function\x94uh\x07}\x94h \x8f\x94(h#h\x13h\x15\x90h"Nubauh \x8f\x94h"Nub.'
-            llm_message = pickle.loads(llm_message)
-            statusCode = StatusCode.SENT_FOR_EXECUTION
-            response = {**data,
-                        **{
-                            "statusCode": StatusCode.REQUEST_SENT_TO_API,
-                        }
-                        }
-            await websocket.send(json.dumps(response))
-            asyncio.create_task(process_user_input(data, websocket, llm_message, statusCode))
-
-            # response_container = LLMResponse()
-            # response_container.set_func_name("message_by_contact_name")
-            # response_container.set_func_args(json.dumps({"name": "Anastasiia 🌸",
-            #                                              "body": "I love you to the moon and back!"}))
-            #
-            # data = {**data,
-            #         **{
-            #             "llmResponse": f"{response_container}",
-            #             "statusCode": StatusCode.SENT_FOR_EXECUTION,
-            #         }
-            #         }
-            # print("Sending: ", data)
-            # await websocket.send(json.dumps(data))
-            # print("SENT")
-            # asyncio.create_task(
-            #     ExecutionHandler.execute_code_asynchronously(
-            #         ResponseParser.get_func_class(response_container.func_name),
-            #         response_container.get_formatted_args(), data, websocket
-            #     )
-            # )
+        if statusCode != StatusCode.ASK_ALL_SAVED:
+            submit_func = func_mapping[statusCode]
+            await submit_func(data, websocket)
 
 
 async def process_user_input(data, websocket, llm_message=None, specified_status=None):
